2f27cdcbf897b8d02586a8ad963eda7287f8370e
Sumit Maurya <thesumit67@gmail.com>
2018-05-18 21:42:36 +0530

    integrated code....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule , Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule , Validators } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello.component';
-
-import { RouterModule, Routes } from '@angular/router';
 import { RegFormComponent } from './reg-form/reg-form.component';
-import { RegFormService } from './reg-form.service';
 import { RegFormStep2Component } from './reg-form-step2/reg-form-step2.component';
+
+import { RegFormService } from './reg-form.service';
+
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/' },
@@ -21,7 +22,8 @@ const routes: Routes = [
       { path: '', component: RegFormComponent },
     ]
   }
-]
+];
+
 
 @NgModule({
   imports:      [
