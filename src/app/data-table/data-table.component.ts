1209f7da6fe2f5e1fa4f8ed14584b169c852a80a
Sumit Maurya <thesumit67@gmail.com>
2018-06-08 17:08:24 +0530

    jjjj"

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -10,10 +10,13 @@ import { DatatableComponent } from '@swimlane/ngx-datatable';
 export class DataTableComponent implements OnInit {
 
   rows;
+  temp;
+
   @ViewChild('dt') dataTable: DatatableComponent;
 
   constructor( private dtService: DataTableService ) {
     this.dtService.getData().subscribe(data => {
+      this.temp = [...data];
       this.rows = data;
     });
   }
@@ -24,6 +27,21 @@ export class DataTableComponent implements OnInit {
 
   changeLimit (value) {
     this.dataTable.limit = Number(value);
-    this.dataTable.recalculate();
+    this.rows = [...this.rows];
   }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.dataTable.offset = 0;
+  }
+
 }
