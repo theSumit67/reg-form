0c863f4e01bf4305fb4d60d592e9766173b6ca77
Sumit Maurya <thesumit67@gmail.com>
2018-06-11 17:46:11 +0530

    row delete & edit WIP"

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, Inject } from '@angular/core';
 import { DataTableService } from './data-table.service';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-data-table',
@@ -11,18 +12,24 @@ export class DataTableComponent implements OnInit {
 
   rows;
   temp;
+  columns = [
+    { name: 'Name' },
+    { name: 'Gender' },
+    { name: 'Age' }
+  ];
+  isChecked = true;
 
   @ViewChild('dt') dataTable: DatatableComponent;
 
   constructor( private dtService: DataTableService ) {
     this.dtService.getData().subscribe(data => {
-      this.temp = [...data];
+      this.temp = data[0];
       this.rows = data;
     });
   }
 
   ngOnInit() {
-    // console.log( this.dataTable );
+    console.log( this.dataTable );
   }
 
   changeLimit (value) {
@@ -42,6 +49,60 @@ export class DataTableComponent implements OnInit {
     this.rows = temp;
     // Whenever the filter changes, always go back to the first page
     this.dataTable.offset = 0;
+  }
+
+  toggleCol() {
+    this.isChecked = this.isChecked ? false : true;
+
+    if (this.isChecked) {
+      this.columns = this.columns.filter(c => {
+        return c.name !== 'Company';
+      });
+    } else {
+      this.columns = [...this.columns, { name: 'Company' }];
+    }
+
+  }
+
+  editRow(row): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+
+  }
+
+  deleteRow( row, rowIndex ) {
+
+    // console.log ( 'deleting . . . '  +  this.rows[rowIndex] );
+    // delete this.rows[rowIndex];
+    this.rows = this.rows.filter( r => {
+      return row.name !== r.name;
+    });
+
+  }
+
+}
+
+//
+
+@Component({
+  selector: 'dialog-row-edit',
+  templateUrl: 'dialog-row-edit.html',
+})
+export class DialogRowEdit {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogRowEdit>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
